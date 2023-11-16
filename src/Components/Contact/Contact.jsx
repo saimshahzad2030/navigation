ed107315237932c572638872d95dad7717893270
saimshehzad2030 <saimshehzad2030@gmail.com>
2023-11-16 22:34:34 +0500

    first commment

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ConditionalRendering from './ConditionalRendering'
+function Contact() {
+  return (
+    <>
+    <div>This is Contact</div>
+    <ConditionalRendering/>
+    </>
+  )
+}
+
+export default Contact
